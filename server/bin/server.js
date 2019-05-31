91493c366853e2b84561a0fae04189397b190317
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2019-05-31 19:02:03 +0300

    Small changes , changed favicon and added README

--- server/bin/server.js
+++ server/bin/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Expresss' });
+  res.send({ express: 'Hello From Express' });
 });
 app.post('/api/world', (req, res) => {
   console.log(req.body);
