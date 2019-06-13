f9b19efcac397246fbf238107ae33bd41da91c3c
Valery Piniazik <valery.piniazik@itechart-group.com>
2019-06-14 02:20:20 +0300

    RLP - login page has been refactored

--- client/src/pages/auth/login/Login.js
+++ client/src/pages/auth/login/Login.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import { Paper, Avatar, Form, Button, Link, GoogleLoginButtonWrapper } from './LoginStyledComponents';
+import { GoogleLoginButton } from '../../../components/google/loginButton'
 import axios from 'axios';
 
 class Login extends Component {
@@ -6,12 +15,12 @@ class Login extends Component {
         super(props);
         this.state = {
             email: '',
-            password: '',
-            showWrongEmailOrPasswordMessage: false,
+            password: ''
         };
         this.handleEmailTyping = this.handleEmailTyping.bind(this);
         this.handlePasswordTyping = this.handlePasswordTyping.bind(this);
         this.onLogin = this.onLogin.bind(this);
+        this.goToRegisterPage = this.goToRegisterPage.bind(this);
     }
 
     handleEmailTyping(event) {
@@ -33,32 +42,74 @@ class Login extends Component {
         })
             .then(() => {
                 this.props.history.push('/home')
-            })
-            .catch(error => {
-                this.setState({
-                    showWrongEmailOrPasswordMessage: true,
-                });
             });
+    }
+
+    goToRegisterPage() {
+        this.props.history.push('/user/register');
     }
 
     render() {
         return (
-            <div id="loginForm">
-                <h4>Login</h4>
-                {this.state.showWrongEmailOrPasswordMessage ? <h6 className="warning_message">Wrong email of password</h6> : undefined}
-                <ul id="formFields">
-                    <li>
-                        <input onChange={this.handleEmailTyping} placeholder="Email" />
-                    </li>
-                    <li>
-                        <input onChange={this.handlePasswordTyping} placeholder="Password" />
-                    </li>
-                </ul>
-                <button onClick={this.onLogin} >Login user</button>
-                <a href="http://localhost:5000/api/user/auth/google">
-                    <button>Login via google</button>
-                </a>
-            </div>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Paper>
+                    <Avatar>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Form noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            onChange={this.handleEmailTyping}
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            onChange={this.handlePasswordTyping}
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            onClick={this.onLogin}
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                        >
+                            Sign In
+                        </Button>
+                        <GoogleLoginButtonWrapper>
+                            <a href="http://localhost:5000/api/user/auth/google">
+                                <GoogleLoginButton />
+                            </a>
+                        </GoogleLoginButtonWrapper>
+                        <Grid container>
+                            <Grid item>
+                                <Link onClick={this.goToRegisterPage} variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Form>
+                </Paper>
+                <Box mt={5}>
+                </Box>
+            </Container>
         );
     }
 }
