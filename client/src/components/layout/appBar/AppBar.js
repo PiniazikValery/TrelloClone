5d71012177990c89b279dae30dc4a3771428818a
Valery Piniazik <valery.piniazik@itechart-group.com>
2019-06-17 00:26:59 +0300

    ILE - integrated log out endpoint

--- client/src/components/layout/appBar/AppBar.js
+++ client/src/components/layout/appBar/AppBar.js
@@ -5,6 +5,8 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import { RootBar, Title, AppBar } from './AppBarStyledComponents';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 class _AppBar extends Component {
     constructor(props) {
@@ -14,6 +16,7 @@ class _AppBar extends Component {
         };
         this.handleMenu = this.handleMenu.bind(this);
         this.handleClose = this.handleClose.bind(this);
+        this.handleLogOutClick = this.handleLogOutClick.bind(this);
     }
 
 
@@ -27,6 +30,14 @@ class _AppBar extends Component {
         this.setState({
             anchorEl: null
         });
+    }
+
+    handleLogOutClick() {
+        axios.post('/api/user/logout')
+            .then(() => {
+                Cookies.set('isAuthenticated', false);
+                this.props.history.push('/user/login');
+            });
     }
 
     render() {
@@ -63,7 +74,7 @@ class _AppBar extends Component {
                                 onClose={this.handleClose}
                             >
                                 <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                                <MenuItem onClick={this.handleClose}>Log out</MenuItem>
+                                <MenuItem onClick={this.handleLogOutClick}>Log out</MenuItem>
                             </Menu>
                         </div>
                     </Toolbar>
