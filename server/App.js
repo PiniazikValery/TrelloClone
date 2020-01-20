573d8c27b66a385b755fbb13dadca040ddf460fd
valerypiniazik <>
2020-01-20 23:10:03 +0300

    preparing for prod

--- server/App.js
+++ server/App.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const path = require('path');
 const passport = require('passport');
 const http = require("http");
 const session = require('express-session');
